80dec2fef69bc7d85bf32e48bd9ac9e0fd18b295
Spahich <artem.egorov@eventplatform.ru>
2020-08-26 12:54:38 +0300

    Add typescript

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import './App.sass';
 
 function App() {
+  const logo = require("../logo.svg") as string;
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import * as serviceWorker from './serviceWorker';
+import './index.sass';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
