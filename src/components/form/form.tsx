8bde6acacc5f1182c7f45b7fe6cee351da6a269e
Spahich <artem.egorov@eventplatform.ru>
2020-08-28 18:10:46 +0300

    Prepare changes

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 import './form.sass';
-import {InjectedFormProps, reduxForm} from 'redux-form';
+import {formValueSelector, InjectedFormProps, reduxForm} from 'redux-form';
 import {Form} from 'react-bootstrap';
+import {connect} from "react-redux";
 
-class TestForm extends React.Component<InjectedFormProps, {[state: string]: any}>{
-  constructor(props: any){
-    super(props);
+let TestForm = (props: any) => {
+  // constructor(props: any){
+  //   super(props);
+  //
+  //   this.state = {
+  //     salary: 40000,
+  //     salaryFormat: '40 000',
+  //     type: 'month',
+  //     tax: true
+  //   };
+  //
+  //   this.handleChange = this.handleChange.bind(this);
+  // }
 
-    this.state = {
-      salary: 40000,
-      salaryFormat: '40 000',
-      type: 'month',
-      tax: true
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  types = [
+  const types = [
     {
       label: 'Оклад за месяц',
       value: 'month'
@@ -36,28 +37,36 @@ class TestForm extends React.Component<InjectedFormProps, {[state: string]: any}
     }
   ];
 
-  setType(type: string){
-    this.setState({
-      type: type
-    });
-  }
+  // setType(type: string){
+  //   this.setState({
+  //     type: type
+  //   });
+  // }
 
-  setTax(){
-    this.setState({
-      tax: !this.state.tax
-    });
-  }
+  // setTax(){
+  //   this.setState({
+  //     tax: !this.state.tax
+  //   });
+  // }
 
-  handleChange(event: any){
-    const salary = event.target.value.split(' ').join('');
+  // handleChange(event: any){
+  //   const salary = event.target.value.split(' ').join('');
+  //
+  //   this.setState({
+  //     salary: isNaN(salary) ? 0 : +salary,
+  //     salaryFormat: isNaN(salary) ? 0 : this.formatNumber(salary)
+  //   });
+  // }
 
-    this.setState({
-      salary: isNaN(salary) ? 0 : +salary,
-      salaryFormat: isNaN(salary) ? 0 : this.formatNumber(salary)
-    });
-  }
+  // getSalary(){
+  //   return this.state.tax ? this.state.salary : (this.state.salary * 0.87).toFixed();
+  // }
+  //
+  // getSalaryWithTax(){
+  //   return this.state.tax ? (this.state.salary / 0.87).toFixed() : this.state.salary;
+  // }
 
-  formatNumber(number: any){
+  const formatNumber = (number: any) => {
     let arr = number.toString().split('').reverse();
     let res: any[] = [];
 
@@ -71,81 +80,91 @@ class TestForm extends React.Component<InjectedFormProps, {[state: string]: any}
     return res.reverse().join('');
   }
 
-  getSalary(){
-    return this.state.tax ? this.state.salary : (this.state.salary * 0.87).toFixed();
-  }
+  return (
+    <Form>
+      <div className="text">Сумма</div>
 
-  getSalaryWithTax(){
-    return this.state.tax ? (this.state.salary / 0.87).toFixed() : this.state.salary;
-  }
+      {/*{*/}
+        {/*types.map(type => (*/}
+          {/*<div className="radio-container" key={type.value}*/}
+               {/*onClick={() => this.setType(type.value)}>*/}
+            {/*<div className={'radio ' + (this.state.type === type.value && 'checked')}/>*/}
+            {/*<div className="label">*/}
+              {/*{type.label}{type.value === 'mrot' && <Notice/>}*/}
+            {/*</div>*/}
+          {/*</div>*/}
+        {/*))*/}
+      {/*}*/}
 
-  render(){
-    return (
-      <Form>
-        <div className="text">Сумма</div>
+      {/*<div className="tax-container">*/}
+        {/*<div className={'tax ' + (!this.state.tax && 'checked')}*/}
+             {/*onClick={() => this.setTax()}>*/}
+          {/*Указать с НДФЛ*/}
+        {/*</div>*/}
 
-        {
-          this.types.map(type => (
-            <div className="radio-container" key={type.value}
-                 onClick={() => this.setType(type.value)}>
-              <div className={'radio ' + (this.state.type === type.value && 'checked')}/>
-              <div className="label">
-                {type.label}{type.value === 'mrot' && <Notice/>}
-              </div>
-            </div>
-          ))
-        }
+        {/*<Form.Check type="switch">*/}
+          {/*<Form.Check.Input type="checkbox"*/}
+                            {/*checked={this.state.tax}*/}
+                            {/*onChange={() => this.setTax()}/>*/}
+          {/*<Form.Check.Label/>*/}
+        {/*</Form.Check>*/}
 
-        <div className="tax-container">
-          <div className={'tax ' + (!this.state.tax && 'checked')}
-               onClick={() => this.setTax()}>
-            Указать с НДФЛ
-          </div>
+        {/*<div className={'tax ' + (this.state.tax && 'checked')}*/}
+             {/*onClick={() => this.setTax()}>*/}
+          {/*Без НДФЛ*/}
+        {/*</div>*/}
+      {/*</div>*/}
 
-          <Form.Check type="switch">
-            <Form.Check.Input type="checkbox"
-                              checked={this.state.tax}
-                              onChange={() => this.setTax()}/>
-            <Form.Check.Label/>
-          </Form.Check>
+      {/*<div className="input-container">*/}
+        {/*<Form.Control type="text"*/}
+                      {/*value={this.state.salaryFormat}*/}
+                      {/*onChange={this.handleChange}/> &#8381;*/}
+      {/*</div>*/}
 
-          <div className={'tax ' + (this.state.tax && 'checked')}
-               onClick={() => this.setTax()}>
-            Без НДФЛ
-          </div>
-        </div>
-
-        <div className="input-container">
-          <Form.Control type="text"
-                        value={this.state.salaryFormat}
-                        onChange={this.handleChange}/> &#8381;
-        </div>
-
-        {
-          this.state.type === 'month' &&
-          <div className="result-container">
-            <div className="result">
-              <strong>{this.formatNumber(this.getSalary())} &#8381; </strong>
-              сотрудник будет получать на руки
-            </div>
-            <div className="result">
-              <strong>{this.formatNumber(this.getSalaryWithTax() - this.getSalary())} &#8381; </strong>
-              НДФЛ, 13% от оклада
-            </div>
-            <div className="result">
-              <strong>{this.formatNumber(this.getSalaryWithTax())} &#8381; </strong>
-              за сотрудника в месяц
-            </div>
-          </div>
-        }
-      </Form>
-    );
-  }
+      {/*{*/}
+        {/*this.state.type === 'month' &&*/}
+        {/*<div className="result-container">*/}
+          {/*<div className="result">*/}
+            {/*<strong>{this.formatNumber(this.getSalary())} &#8381; </strong>*/}
+            {/*сотрудник будет получать на руки*/}
+          {/*</div>*/}
+          {/*<div className="result">*/}
+            {/*<strong>{this.formatNumber(this.getSalaryWithTax() - this.getSalary())} &#8381; </strong>*/}
+            {/*НДФЛ, 13% от оклада*/}
+          {/*</div>*/}
+          {/*<div className="result">*/}
+            {/*<strong>{this.formatNumber(this.getSalaryWithTax())} &#8381; </strong>*/}
+            {/*за сотрудника в месяц*/}
+          {/*</div>*/}
+        {/*</div>*/}
+      {/*}*/}
+    </Form>
+  );
 }
 
-export default reduxForm({
-  form: 'text-form'
-})(TestForm);
+
+TestForm = reduxForm({
+  form: 'test-form'
+})(TestForm)
+
+const selector = formValueSelector('test-form')
+
+TestForm = connect(
+  state => {
+    // can select values individually
+    const hasEmailValue = selector(state, 'hasEmail')
+    const favoriteColorValue = selector(state, 'favoriteColor')
+    // or together as a group
+    const {firstName, lastName} = selector(state, 'firstName', 'lastName')
+    return {
+      hasEmailValue,
+      favoriteColorValue,
+      fullName: `${firstName || ''} ${lastName || ''}`
+    }
+  }
+)(TestForm)
+
+export default TestForm
 
 class Notice extends React.Component<any, {[state: string]: any}>{
   constructor(props: any){
